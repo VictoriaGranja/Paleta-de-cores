06cb99e36bb5f7056fea7eaefebea86497e4f84f
VictoriaGranja <victoriagranja@id.uff.br>
2022-09-09 15:11:36 -0300

    Criando estrutura da paleta de cores

--- paleta.js
+++ paleta.js
@@ -1,6 +1,16 @@
-const colorInput = document.getElementById('color')
-colorInput.addEventListener('input', () => {
-    const colorCode = document.getElementById('color').value;
-    document.querySelector('.wrapper').style.backgroundColor = colorCode
-    document.querySelector('label').style.backgroundColor = colorCode
-})
+(function(){
+    const colorInput = document.getElementById('color')
+    colorInput.addEventListener('input', () => {
+        const colorCode = document.getElementById('color').value;
+        console.log(typeof(colorCode))
+        document.querySelector('.wrapper').style.backgroundColor = colorCode
+        document.querySelector('label').style.backgroundColor = colorCode
+        corPrincipal(colorCode)
+    })
+    
+})()
+
+function corPrincipal(corAtual){
+    let corOrginal = document.querySelector('.cor3')
+    corOrginal.style.backgroundColor = corAtual
+}
